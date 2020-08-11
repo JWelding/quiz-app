c3400acab2fde92bfd3c97eaf8306c72179320d4
Jeff Welding <jweld14@gmail.com>
2020-08-11 12:58:53 -0400

    added seperate html files

--- script.js
+++ script.js
@@ -1,13 +1,65 @@
-const QUIZ={}
+const QUIZ= [
+    {
+        question: "The plural of 'moose' is?",
+        answers: {
+            a: "moosen"
+            b: "moose"
+            c: "mooses"
+            d: "meese"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "How many continents are there?",
+        answers: {
+            a: "5"
+            b: "8"
+            c: "7"
+            d: "11"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What causes tides?",
+        answers: {
+            a: "wind"
+            b: "ships"
+            c: "whales"
+            d: "moon"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Who invented the light bulb?",
+        answers: {
+            a: "Thomas Edison"
+            b: "Bill Gates"
+            c: "General Electric"
+            d: "Ben Franklin"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "How many legs does a spider have?",
+        answers: {
+            a: "4"
+            b: "6"
+            c: "8"
+            d: "12"
+        },
+        correctAnswer: "c"
+    }
+];
 
 //helper functions
 //these fuction do misc. tasks
+
 
 //generator functions
 //these function make html strings
 function generateStartPage(){
 return `
-<p>discribe quiz...</p>
+<p>Here is a short and quick way for you see if you are capable of arguing with a 1st grader.  Click start if you dare!</p>
 <button>Start Quiz</button>
 `
 }
